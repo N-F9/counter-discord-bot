04c51757190991a2387c1b5f2e179a24c377050f
Nick <ndferguson@protonmail.com>
2020-07-02 16:39:40 -0400

    💻 | Making the counter part

--- commands/reset.js
+++ commands/reset.js
@@ -8,8 +8,6 @@ module.exports = {
 	name: 'reset',
 	description: 'Resets the counter',
 	execute(message, args) {
-    const data = db.get("guilds").find({ guildId: message.guild.id }).value()
-
     if (!message.member.hasPermission('ADMINISTRATOR')) {
       error("Insufficient Permissions, the guild owner is the only one that can run this command!", message.channel)
       return
--- index.js
+++ index.js
@@ -13,19 +13,19 @@ const db = low(adapter)
 db.defaults({ guilds: [] })
   .write()
 
-db.get('guilds')
-  .remove()
-  .write()
+// db.get('guilds')
+//   .remove()
+//   .write()
 
 // Gets the commands
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`)
+	client.commands.set(command.name, command)
 }
 
 // Loads the bot
@@ -37,16 +37,30 @@ client.once('ready', () => {
 // Listens for messages
 
 client.on('message', message => {
+  // Commands
   let prefix = "-"
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-  
-  for (const c of client.commands) {
-    if (command === c[1].name) {
-      client.commands.get(c[1].name).execute(message, args);
+	if (message.content.startsWith(prefix) || message.author.bot) {
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase();
+    
+    for (const c of client.commands) {
+      if (command === c[1].name) {
+        client.commands.get(c[1].name).execute(message, args)
+      }
     }
+    return
+  }
+
+  // Counter
+  let data = db.get("guilds").find({ guildId: message.guild.id }).value()
+	if (message.channel.id == db.get('guilds').find({ guildId: message.guild.id }).value()) {
+    if (message.content == db.get('guilds').find({ guildId: message.guild.id }).value()) {
+      db.get('guilds')
+        .find({ guildId: message.guild.id })
+        .update('count', n => n + 1)
+        .write()
+    }
+    message.channel.setTopic(`Current Number: ${db.get('guilds').find({ guildId: message.guild.id }).value()}`)
   }
 })
 
